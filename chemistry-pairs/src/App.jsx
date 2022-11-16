109f91948e3b660e558749dc7d233bd5a4c16501
laurie60 <laurie8000@googlemail.com>
2022-11-16 15:25:36 +0000

    attempt to get alternative image on hover

--- chemistry-pairs/src/App.jsx
+++ chemistry-pairs/src/App.jsx
@@ -2,17 +2,82 @@ import "./App.css";
 import Layout from "./components/Layout";
 import logo from "./assets/chemistry.svg";
 import chemistryCard from "./assets/chemistrycard.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ethanol from "./assets/ethanol.svg";
 import paracetamol from "./assets/paracetamol.svg";
-
+import altParacetamol from "./assets/altParacetamol.svg";
+import altEthanol from "./assets/altEthanol.svg";
 function App() {
+  const handleOnMouseOver = (index) => {
+    console.log(index, "index in handle mouse");
+  };
+
+  const sourceFTN = (index) => {
+    console.log(index, hover, "index, hover");
+    if (flipped.includes(index) || found.includes(index)) {
+      return chemPairs[index].image;
+    }
+    if (hover) {
+      console.log("TRUE");
+      return chemPairs[index].altImage;
+    } else return chemistryCard;
+  };
+  function shuffle(array) {
+    return array.sort(() => Math.random() - 0.5);
+  }
+
   const [size, setSize] = useState(4);
+  const [flipped, setFlipped] = useState([]);
+  const [chemPairs, setChemPairs] = useState([]);
+  const [found, setFound] = useState([]);
+  const [allowFlip, setAllowFlip] = useState(true);
+  const [hover, setHover] = useState("");
+
+  console.log(flipped, "flipped");
+
+  useEffect(() => {
+    // const chemicals = [
+    //   { image: ethanol, altImage: altEthanol },
+    //   { image: paracetamol, altImage: altParacetamol },
+    // ];
+    const chemicals = [
+      { image: ethanol, altImage: altEthanol },
+      { image: paracetamol, altImage: altParacetamol },
+    ];
+    console.log(chemicals, "chemicals in useEffect");
+
+    setChemPairs(shuffle(chemicals.concat(chemicals)));
+  }, []);
+
+  useEffect(() => {
+    if (
+      flipped.length === 2 &&
+      chemPairs[flipped[0]] === chemPairs[flipped[1]]
+    ) {
+      setFlipped([]);
+      setFound([...found, ...flipped]);
+    }
+    if (flipped.length === 2) {
+      setAllowFlip(false);
+      let timer1 = setTimeout(() => setFlipped([]), 1000);
+      return () => {
+        setAllowFlip(true);
+        clearTimeout(timer1);
+      };
+    }
+  }, [flipped, chemPairs, found]);
+
   const array = [];
   for (let i = 0; i < size; i++) {
     array.push(chemistryCard);
   }
-  console.log(array);
+
+  // console.log(array);
+  const handleOnClick = (index) => {
+    if (allowFlip && !flipped.includes(index)) {
+      setFlipped([...flipped, index]);
+    }
+  };
   return (
     <Layout>
       <section className="game-container">
@@ -21,14 +86,29 @@ function App() {
             size === 4 ? "four" : ""
           }${size === 5 ? "five" : ""} pair-grid`}
         >
-          {array.map((image, index) => {
+          {array.map((backImg, index) => {
+            chemPairs.length &&
+              console.log(chemPairs[index].image, "CHEM PAIRS image");
+            // chemPairs.length && setDynImage(chemPairs[index].image);
+
+            // console.log(dynImage);
             return (
               <>
+                {/* {console.log(hover, "DYN IMAGE", chemPairs[index].altImage)} */}
+                {console.log(hover)}
                 <img
+                  onMouseEnter={() => {
+                    handleOnMouseOver(index);
+                    // if (flipped.includes(index) || found.includes(index)) {
+                    //   setHover(index);
+                    // }
+                  }}
+                  // onMouseOut={() => setHover("")}
                   className="grid-img"
                   key={index}
-                  src={image}
+                  src={sourceFTN(index)}
                   alt="Chemistry pairs logo"
+                  onClick={() => handleOnClick(index)}
                 ></img>
               </>
             );
